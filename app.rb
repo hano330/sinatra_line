57208765ef59394fd6e552188071ff5bf03fee5b
Masa <masayoshiasus1@gmail.com>
2017-05-24 17:15:41 +0900

    herokuが動かない

--- app.rb
+++ app.rb
@@ -3,26 +3,6 @@ Bundler.require
 
 require "rack-flash"
 
-class User < ActiveRecord::Base
-  has_secure_password
-
-  #Validation
-  validates :name, presence: true
-end
-
-class Post < ActiveRecord::Base
-end
-
-class Friend < ActiveRecord::Base
-end
-
-class Fadd < ActiveRecord::Base
-end
-
-class Photo < ActiveRecord::Base
-end
-
-
 class MineApp < Sinatra::Base
   configure do
     # DB設定ファイルの読み込み
@@ -200,3 +180,23 @@ class MineApp < Sinatra::Base
     erb :mypage
   end
 end
+
+class User < ActiveRecord::Base
+  has_secure_password
+
+  #Validation
+  validates :name, presence: true
+end
+
+class Post < ActiveRecord::Base
+end
+
+class Friend < ActiveRecord::Base
+end
+
+class Fadd < ActiveRecord::Base
+end
+
+class Photo < ActiveRecord::Base
+end
+
